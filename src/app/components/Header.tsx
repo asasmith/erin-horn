679084523708039e2c255998e07492736ca10ba8
asasmith <smith.asa.la@gmail.com>
2025-02-10 08:38:07 -0500

    fix typo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
                 <Link href="/about" className={`mx-2 font-sans ${isAboutPath ? 'underline' : ''}`}>About</Link>
                 <Link href="/services" className={`mx-2 font-sans ${isServicesPath ? 'underline' : ''}`}>Services</Link>
             </nav>
-            <Link href="/" className="font-serif font-semibold text-4xl text-secondary capitalize">blissfull balance</Link>
+            <Link href="/" className="font-serif font-semibold text-4xl text-secondary capitalize">blissful balance</Link>
             <Link href="/services">
                 <button className="px-4 py-2 bg-primary font-sans text-white rounded">Book an Appointment</button>
             </Link>
