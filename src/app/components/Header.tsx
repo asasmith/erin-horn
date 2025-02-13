891f60def2db66c7cda0598ea2ed124b15ee297d
asasmith <smith.asa.la@gmail.com>
2025-02-12 19:38:39 -0500

    update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,24 @@
 'use client'
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import Logo from '../../../public/logo.png';
 
 export const Header = () => {
     const pathname = usePathname();
     const isAboutPath = pathname === '/about' ? 'underline' : '';
 
     return (
-        <header className="py-4 flex justify-between container mx-auto">
-            <nav className="flex items-center text-offBlack">
-                <Link href="/about" className={`mx-2 font-sans ${isAboutPath ? 'underline' : ''}`}>About</Link>
-            </nav>
-            <Link href="/" className="font-serif font-semibold text-4xl text-secondary capitalize">blissful balance</Link>
-            <Link href="/book">
-                <button className="px-4 py-2 bg-primary font-sans text-white rounded">Book an Appointment</button>
+        <header className="py-4 flex justify-between items-center container mx-auto">
+            <Link href="/" className="">
+                <Image src={Logo} alt="Blissful Balance Logo" className="w-32" />
             </Link>
+            <div className="flex items-center justify-between w-80">
+                <Link href="/about" className={`mx-2 font-sans ${isAboutPath ? 'underline' : ''}`}>About</Link>
+                <Link href="/book" className="px-4 py-2 bg-primary font-sans text-white rounded">
+                    Book an Appointment
+                </Link>
+            </div>
         </header>
     )
 }
